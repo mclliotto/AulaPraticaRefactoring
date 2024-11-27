dc5acb7757c0093ff2873185f4be47393189cce5
mclliotto <mclliotto@minha.fag.edu.br>
2024-11-27 00:14:51 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -1,32 +1,43 @@
 public class Movie {
-    
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
-    }
-
-    public int getPriceCode() {
-        return _priceCode;
-    }
-
-    public void setPriceCode(int arg) {
-        _priceCode = arg;
+        setPriceCode(priceCode);
     }
 
     public String getTitle() {
         return _title;
     }
 
+    public int getPriceCode() {
+        return _price.getPriceCode();
+    }
+
+    public void setPriceCode(int priceCode) {
+        switch (priceCode) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
+    }
+
     public double getCharge(int daysRented) {
         double result = 0;
-        switch (_priceCode) {
+        switch (getPriceCode()) {
             case REGULAR:
                 result += 2;
                 if (daysRented > 2) {
@@ -44,5 +55,12 @@ public class Movie {
                 break;
         }
         return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if (getPriceCode() == NEW_RELEASE && daysRented > 1) {
+            return 2;
+        }
+        return 1;
     }
 }
