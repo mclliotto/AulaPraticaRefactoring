8a08a2f2043106792dc98b7803bcf6398d0897fe
mclliotto <mclliotto@minha.fag.edu.br>
2024-11-26 23:43:23 -0300

    Commit 3

--- Customer.java
+++ Customer.java
@@ -25,7 +25,7 @@ public class Customer {
 
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            double thisAmount = amountFor(each); // Chamada do novo método
+            double thisAmount = amountFor(each); // Chamada do novo método com o parâmetro renomeado
 
             // Adicionar pontos de locatário frequente
             frequentRenterPoints++;
@@ -44,23 +44,23 @@ public class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
+    private double amountFor(Rental aRental) { // Parâmetro renomeado
         double thisAmount = 0;
 
-        switch (each.getMovie().getPriceCode()) {
+        switch (aRental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2) {
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (aRental.getDaysRented() > 2) {
+                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
                 }
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += aRental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3) {
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (aRental.getDaysRented() > 3) {
+                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
                 }
                 break;
         }
