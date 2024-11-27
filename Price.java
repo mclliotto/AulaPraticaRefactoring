6c682cd9416b73d5e5c15e99d2c0a520aff82254
mclliotto <mclliotto@minha.fag.edu.br>
2024-11-27 00:26:42 -0300

    Commit 13

--- Price.java
+++ Price.java
@@ -1,3 +1,7 @@
 public abstract class Price {
+    public abstract int getPriceCode();
+
     public abstract double getCharge(int daysRented);
+
+    public abstract int getFrequentRenterPoints(int daysRented); 
 }
